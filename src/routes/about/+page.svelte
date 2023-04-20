0eb4537aa39f37f8075ab24b807a74b1033172db
jacktho <jack@jacktho.com>
2023-04-20 18:26:45 -0400

    Created initial about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+</script>
+
+<div class="px-6 py-32 lg:px-8">
+	<div class="mx-auto max-w-3xl text-base leading-7 text-gray-200">
+		<p class="text-base font-semibold leading-7 text-orange-600">Jack Thompson</p>
+		<h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">
+			Passionate Web Developer and Tech Enthusiast
+		</h1>
+		<p class="mt-6 text-xl leading-8">
+			Embodying a lifelong passion for technology, I have cultivated a diverse skill set in web
+			development, tackling complex challenges.
+		</p>
+		<div class="max-w-2xl">
+			<p class="mt-8">
+				With a love for technology that dates back to 1994, I have been fascinated with the internet
+				and its potential since my early years. My journey as a web developer began in 1999 when I
+				created a website for sharing music downloads. Over the years, I have honed my skills in
+				various technologies such as PHP, JavaScript, Angular, Node.js, Svelte, and SvelteKit.
+			</p>
+			<p class="mt-8">
+				My diverse professional experiences have included a role as a QA test engineer working on
+				early mobile applications, running my own web development and IT support business, and
+				co-founding two startups in the insurance and financial advisory industries. These
+				experiences have allowed me to develop a broad range of skills and expertise, including app
+				development, project management, and team leadership.
+			</p>
+			<p class="mt-8">
+				As the CTO of our current startup, I have built several applications that streamline
+				processes for our clients in the financial advisory sector. My expertise in creating
+				user-friendly applications and finding innovative solutions to complex problems has been a
+				driving force behind our company's success.
+			</p>
+			<p class="mt-8">
+				While my professional journey has taken various paths, my passion for web development has
+				remained constant. I am confident in my ability to create competitive web applications and
+				am eager to take on new challenges.
+			</p>
+			<p class="mt-8">
+				With over 16 years of experience in the tech industry, I am ready to help your company
+				achieve its goals by leveraging my diverse skill set and enthusiasm for innovation.
+			</p>
+		</div>
+	</div>
+</div>
